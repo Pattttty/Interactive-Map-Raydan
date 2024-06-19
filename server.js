4e3e67b2d1b8d70c9b6245ba316b7863a44f4f7a
Patrick <88867382+Pattttty@users.noreply.github.com>
2024-06-19 11:19:58 +0930

    Small changes for website

--- server.js
+++ server.js
@@ -74,6 +74,6 @@ app.post('/delete-marker', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
